ef6e2833f9943f010048077bca10e858bfcdca36
bdilollo <113383715+bdilollo@users.noreply.github.com>
2022-11-29 13:16:21 -0600

    initial files added and webpack.config completed

---
+++ client/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      cards: './src/js/cards.js'
+    },
+
+    // TODO: Add the correct output
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+
+    // TODO: Add the correct plugins
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Contact Directory'
+      }),
+      new MiniCssExtractPlugin(),
+      new WorkboxPlugin.GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Contact Directory',
+        short_name: 'Directory',
+        description: 'Create and store important contacts!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('src', 'icons'),
+          },
+        ],
+      })
+    ],
+
+    // TODO: Add the correct modules
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
+    }
+  };
+};
